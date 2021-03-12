edff0fc428af5154bdc8433bd09ff03e814d4961
Serkan <serkara2de@hotmail.com>
2021-03-12 12:49:47 +0100

    ApiTest Day4

--- src/test/java/apitests/hrApiWithPath.java
+++ src/test/java/apitests/hrApiWithPath.java
@@ -7,6 +7,8 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import utilities.ConfigurationReader;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
 import static org.testng.Assert.*;
 public class hrApiWithPath {
@@ -37,6 +39,38 @@ public class hrApiWithPath {
         String secondCountryName = response.path("items.country_name[1]");
         System.out.println("secondCountryName = " + secondCountryName);
 
+        String hrefLink2 = response.path("items.links[2].href[0]");
+        System.out.println("hrefLink2 = " + hrefLink2);
+
+        //get all countries
+        List<String> countryNames = response.path("items.country_name");
+        System.out.println("countryNames = " + countryNames);
+
+        //assert that all region_ids are equal to 2
+        List<Integer> regionIds = response.path("items.region_id");
+        for (int regionId : regionIds) {
+            System.out.println(regionId);
+            assertEquals(regionId,2);
+        }
+    }
+
+    @Test
+    public void test2(){
+        Response response = given().accept(ContentType.JSON)
+                .and().queryParams("q", "{\"job_id\":\"IT_PROG\"}")
+                .when().get("/employees");
+        assertEquals(response.statusCode(),200);
+        assertEquals(response.contentType(),"application/json");
+        assertTrue(response.body().asString().contains("IT_PROG"));
+
+
+        //make sure we have only IT_PROG as a job_id
+        List<String> jobIDs = response.path("items.job_id");
+        for (String jobID : jobIDs) {
+            assertEquals(jobID,"IT_PROG");
+        }
 
     }
+
+
 }
---
+++ src/test/java/apitests/spartanTestWithJsonPath.java
@@ -0,0 +1,69 @@
+package apitests;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import utilities.ConfigurationReader;
+
+import static io.restassured.RestAssured.*;
+import static org.testng.Assert.*;
+
+public class spartanTestWithJsonPath {
+
+    @BeforeClass
+    public void beforeclass(){
+        baseURI= ConfigurationReader.get("spartan_api_url");
+    }
+
+    /*
+          Given accept type is json
+          And path param spartan id is 11
+          When user sends a get request to /spartans/{id}
+         Then status code is 200
+         And content type is Json
+         And   "id": 11,
+               "name": "Nona",
+              "gender": "Female",
+              "phone": 7959094216
+    */
+
+    @Test
+    public void test1(){
+        Response response = given().accept(ContentType.JSON)
+                .and().pathParam("id", 11)
+                .when().get("/api/spartans/{id}");
+
+        assertEquals(response.statusCode(),200);
+        assertEquals(response.contentType(),"application/json");
+
+
+        //verify id and name with path()
+        int id = response.path("id");
+        String name = response.path("name");
+
+        assertEquals(id,11);
+        assertEquals(name,"Nona");
+
+        String gender = response.path("gender");
+        Object phone = response.path("phone");
+
+        //assign response to JsonPath
+
+        JsonPath jsonPath = response.jsonPath();
+
+        int idJson = jsonPath.getInt("id");
+        String nameJson = jsonPath.getString("name");
+        String genderJson = jsonPath.getString("gender");
+        Object phoneJson = jsonPath.getLong("phone");
+
+
+        assertEquals(idJson,11);
+        assertEquals(nameJson, "Nona");
+        assertEquals(genderJson,"Female");
+        assertEquals(phoneJson,7959094216l);
+
+    }
+
+}
